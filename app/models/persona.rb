b427bc23eac757d9f7549cdad3561d5078440c77
Fco-Alvarez <fco.alvarez@icloud.com>
2022-07-09 23:35:46 -0400

    Update persona.rb

--- app/models/persona.rb
+++ app/models/persona.rb
@@ -1,5 +1,6 @@
 class Persona < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-  has_many: postulaciones, dependent: :destroy
+         :recoverable, :rememberable, :validatable
 end
